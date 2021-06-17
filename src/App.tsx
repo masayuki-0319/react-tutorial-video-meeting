a12815f435cd5a8a9343d0c97a605464fba7b7ad
将之大西 <omccello@gmail.com>
2021-06-17 22:49:40 +0900

    finish step9

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import { VFC } from 'react';
 
+const getMedia = async () => {
+  const constraints: MediaStreamConstraints = { audio: true, video: true };
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getMedia();
+
 const App: VFC = () => {
   return <div>Hello, React!</div>;
 };
